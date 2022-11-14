f481576ef5fdfc14c453ab215cb63140f300b14a
Gregorio Alasino <gregoalasino@gmail.com>
2022-11-14 18:02:21 -0300

    touching hys

--- src/app/service/image.service.ts
+++ src/app/service/image.service.ts
@@ -30,3 +30,27 @@ export class ImageService {
     .catch(error => console.log(error))
   }
 }
+
+/*uploadImage(event){
+    // si se eligió la imagen
+    if (event.target.files.length !== 0 || event.target.files[0] != null )
+     {
+      // obtengo el archivo completo de la img (nombre, tipo, tamaño, etc..)
+      const file = event.target.files[0];
+      // obtengo solo el nombre de la imagen
+      const name = file.name;
+      const fileRef = this.storage.ref(name);
+      // subo imagen a firestorage con el nombre y todas sus prop(tipo, tamaño, etc..)
+      const task = this.storage.upload(name, file);
+      task.snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.image$ = fileRef.getDownloadURL();
+          this.image$.subscribe(url => {
+            this.selectedFile = url;
+        });
+      })
+    )
+    .subscribe();
+    }
+  }*/
